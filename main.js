12a14f9a132bf74e4087ac2bac0a031d17ad3a04
manikandan250 <manimani456m@gmail.com>
2023-01-03 16:22:44 +0530

    swiggy

---
+++ main.js
@@ -0,0 +1,120 @@
+let foods=[
+{
+	id:1,
+	name:"Dindigul Thalappakatti",
+	cuisine:"South Indian, North Indian",
+	imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/x3d6cifz8rw0ssv4jqwk",
+	rating:4.4,
+	time:"34 minits",
+	amount:500
+},
+{
+	id:2,
+	name:"KFC",
+	cuisine:"American, Snacks",
+	imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bdcd233971b7c81bf77e1fa4471280eb",
+	rating:4.1,
+	time:"31 minits",
+	amount:400
+},
+{
+	id:3,
+	name:"Star malabar",
+	cuisine:"South Indian",
+	imgurl:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/s3tg8dldtoi2t8fvodjs",
+	rating:4.2,
+	time:"26 minits",
+	amount:200
+}
+]
+
+let container=document.querySelector(".container");
+
+let card=document.createElement("div");
+	card.classList.add("row")
+	container.appendChild(card);
+	
+foods.forEach(function(e){
+	
+	let div1=document.createElement("div");
+	div1.classList.add("col");
+	card.appendChild(div1);
+	let img=document.createElement("img");
+	img.src=e.imgurl;
+	div1.appendChild(img);
+	
+	let head=document.createElement("h3");
+		head.innerText=e.name;
+		div1.appendChild(head);
+		
+		let h4=document.createElement("h4");
+		h4.innerText=e.cuisine;
+		div1.appendChild(h4);
+		
+		
+		let content=document.createElement("div");
+			content.setAttribute("class","flex");
+			div1.appendChild(content);
+			
+			
+			
+			let stars=document.createElement("div");
+			stars.setAttribute("class","add1");
+			content.appendChild(stars);
+			
+			let icon=document.createElement("i");
+			icon.setAttribute("class","bi bi-star-fill")
+			stars.appendChild(icon);
+			
+			
+			
+			let rat=document.createElement("span");
+			rat.innerText=e.rating;
+			stars.appendChild(rat);
+			
+			
+			
+			let time=document.createElement("div");
+			content.appendChild(time);
+			let ti=document.createElement("h4");
+			ti.innerText=e.time;
+			time.appendChild(ti);
+			
+			let amount=document.createElement("div");
+			content.appendChild(amount);
+			let amt=document.createElement("h4");
+			amt.innerText=e.amount;
+			amount.appendChild(amt);
+			
+			
+			let div2=document.createElement("div");
+			div2.classList.add("incre");
+			div1.appendChild(div2);
+			
+			let btn=document.createElement("button");
+			btn.innerHTML="-";
+			div2.appendChild(btn);
+			
+			btn.addEventListener("click",function(){
+				if(type.value>0){
+					--type.value;
+				}
+			})
+			
+			let type=document.createElement("input");
+			type.value="0";
+			div2.appendChild(type);
+			
+			let incree=document.createElement("button");
+			incree.innerHTML="+";
+			div2.appendChild(incree);
+			
+			incree.addEventListener("click",function(){
+				++type.value;
+			})
+			
+	
+	
+})
+
+	
